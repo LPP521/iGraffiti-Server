d2a7ad39dc7d25bd7e17e7ed62105fd1ec970e23
ilordhalo <ilordhalo@users.noreply.github.com>
2017-07-05 22:18:34 +0800

    新涂鸦扫描进程
    实现功能：将涂鸦与墙面进行拼接操作。向接受墙面更新通知的用户发送更新消息。

--- PasteImageConf.py
+++ PasteImageConf.py
@@ -1,17 +1,25 @@
+#coding=utf-8
 import os,time
 import MySQLdb
 import Image
+from apns import APNs, Frame, Payload
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
+apns = APNs(use_sandbox=True, cert_file='/home/myscript/APNs-Cer/cert2.pem', key_file='/home/myscript/APNs-Cer/key2.pem')
 username=''
 password=''
-def connect(dbname,sqlstring):
-    db=MySQLdb.connect(host="localhost", user=username, passwd=password, db=dbname)
+def connect(dbname,sqlstring,uncmt=0):
+    db=MySQLdb.connect(host="localhost", user=username, passwd=password, db=dbname,charset="utf8")
     c = db.cursor(cursorclass = MySQLdb.cursors.DictCursor)
+    c.execute('SET NAMES \'utf8\';')
     c.execute(sqlstring)
-    db.commit()
+    if uncmt==0:
+        db.commit()
     rows=c.fetchall()
     return rows
 while True:
-    rows=connect("iGraffiti","select ImageID,ImageLocation,WallLocation from PasteQueue inner join Wall on PasteQueue.TargetWallID=Wall.WallID")
+    rows=connect("iGraffiti","select ImageID,ImageLocation,WallName,WallLocation,TargetWallID from PasteQueue inner join Wall on PasteQueue.TargetWallID=Wall.WallID")
     for row in rows:
         try:
             imid=row['ImageID']
@@ -27,6 +35,16 @@ while True:
             bgim.save(walllocation)
             filename=imlocation
             os.remove(filename)
+            notiwallid=row['TargetWallID']
+            notiwallname=row['WallName']
+            trows=connect("iGraffiti","call select_NotiToken('"+str(notiwallid)+"')",1)
+            print(trows)
+            for trow in trows:
+                token_hex=trow["DeviceToken"]
+                if token_hex=="":
+                    continue
+                payload = Payload(alert="你关注的墙有更新！", sound="default", badge=1,custom={'WallID':notiwallid,'WallName':notiwallname})
+                apns.gateway_server.send_notification(token_hex, payload)
         except:
             localtime=time.asctime(time.localtime(time.time()))
             errorlog=localtime+"  "+imlocation+" PasteImage Error -PasteImageConf.py\n"
@@ -36,3 +54,4 @@ while True:
             logfile.close()
         connect("iGraffiti","delete from PasteQueue where ImageId="+str(imid)+"")
     time.sleep(1)
+    
