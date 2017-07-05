84d3953497eb370c1854f3f3716503fb9bb9ad5f
ilordhalo <ilordhalo@users.noreply.github.com>
2017-07-05 21:48:11 +0800

    Update database.py

--- database.py
+++ database.py
@@ -1,8 +1,8 @@
 # -*- coding:utf-8 -*-
 import MySQLdb
 import os
-username='root'
-password='PI*32767*i*e'
+username=''
+password=''
 def connect(dbname,sqlstring,procedure=0):
     db=MySQLdb.connect(host="localhost", user=username, passwd=password, db=dbname,charset="utf8")
     #c=db.cursor()
